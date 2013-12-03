48e241e300c99ae27ca784ed435bb20d6cbebfdc
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-03 14:20:31 -0800

    Created rails app

---
+++ drink_book/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrinkBook::Application.config.secret_key_base = '17a32159a142b870240fe6153a26bcf375e388fd80aee61d34886accfdba725db8f465b00a4c058f261a4d526da552006487f864275c0a6cc784e58ff86bd398'
---
+++ drink_book/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DrinkBook::Application.config.session_store :cookie_store, key: '_drink_book_session'
