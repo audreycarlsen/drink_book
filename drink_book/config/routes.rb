9c9f3103bfc09b733816a4f1a28cae35230ea4fa
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-03 14:39:01 -0800

    Added Ingredient and Tool scaffolds

--- drink_book/config/routes.rb
+++ drink_book/config/routes.rb
@@ -1,4 +1,8 @@
 DrinkBook::Application.routes.draw do
+  resources :tools
+
+  resources :ingredients
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
