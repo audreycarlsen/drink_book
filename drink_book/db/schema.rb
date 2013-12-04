8d09ca10be46035d361807cf0877ce44f96e1bac
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-03 16:55:55 -0800

    Migrated the db

--- drink_book/db/schema.rb
+++ drink_book/db/schema.rb
@@ -11,9 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203223839) do
+ActiveRecord::Schema.define(version: 20131204004843) do
+
+  create_table "ingredient_recipes", force: true do |t|
+    t.integer  "recipe_id"
+    t.integer  "ingredient_id"
+    t.string   "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ingredients", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
